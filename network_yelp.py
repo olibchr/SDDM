44a7e1fbcc11d527824fa75ec02fa061d2c96f3a
oibchr <oliver.becher@gmail.com>
2016-05-28 00:36:18 +0200

    print

--- network_yelp.py
+++ network_yelp.py
@@ -108,7 +108,10 @@ def load_dataset():
     X_train, X_val = X_train[:-size], X_train[-size:]
     y_train, y_val = y_train[:-size], y_train[-size:]
     
-    print(x_test[1:100])
+
+    for items in X_train:
+        print(len(items))
+    
     X_test = np.array(X_test, dtype=theano.config.floatX)
     y_test = np.array(y_test, dtype=np.int32)
 
