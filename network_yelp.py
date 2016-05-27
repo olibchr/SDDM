8976a5b3cae6bb032bfa644a5d70b9afa95597ca
oibchr <oliver.becher@gmail.com>
2016-05-28 00:20:29 +0200

    push

--- network_yelp.py
+++ network_yelp.py
@@ -108,7 +108,6 @@ def load_dataset():
     X_train, X_val = X_train[:-size], X_train[-size:]
     y_train, y_val = y_train[:-size], y_train[-size:]
     
-    print(X_test.shape)
 
     X_test = np.array(X_test, dtype=theano.config.floatX)
     y_test = np.array(y_test, dtype=np.int32)
