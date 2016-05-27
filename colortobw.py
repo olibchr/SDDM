5ce26c1c2875e3c8b2f15918a7c6b67d5c7ff9da
arthurlaurens <alvrooijen@gmail.com>
2016-05-27 20:37:03 +0200

    load images from directory "data"

--- colortobw.py
+++ colortobw.py
@@ -13,7 +13,8 @@ from scipy import misc
 
 # ################## Params ##################
 N_CLASSES = 2  # number of output units
-dirname = 'photos_resized/photos_resized/'
+# dirname = 'photos_resized/photos_resized/'
+dirname = 'data'
 labelfile = 'meta/image_meta.csv'
 
 
@@ -22,11 +23,9 @@ images = [f for f in listdir(dirname) if isfile(join(dirname, f))]
 
 
 for file in images:
-
     imagename = file[:-4]
 
     if file[-3:] == "jpg":
-
         col = Image.open(dirname+file)
         gray = col.convert('L')
         gray.save(dirname+file)
