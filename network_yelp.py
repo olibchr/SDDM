742def75208c2e725ddf51de76806e542afe25fc
arthurlaurens <alvrooijen@gmail.com>
2016-05-27 22:48:37 +0200

    delete break line in file name

--- network_yelp.py
+++ network_yelp.py
@@ -76,7 +76,7 @@ def load_dataset():
     count = 0
 
     for img_id in images:
-        file_path = IMG_DIR + img_id + '.jpg'
+        file_path = IMG_DIR + img_id[:-1] + '.jpg' # -1 to remove break line
         try:
             face = misc.imread(file_path)
             face = face.reshape(-1, 1, IMG_X_SIZE, IMG_Y_SIZE)
