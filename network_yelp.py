6f3008940f43b48632aa23405fc2ca30bcf36958
arthurlaurens <alvrooijen@gmail.com>
2016-05-28 17:27:02 +0200

    load files from base dir

--- network_yelp.py
+++ network_yelp.py
@@ -20,8 +20,8 @@ import mlp
 
 # ################## Params ##################
 N_CLASSES = 2  # number of output units
-IMG_DIR = '../../SDDM/photos_resized/photos_resized/'
-META_DATA_FILE = '../../SDDM/meta/image_meta.csv'
+IMG_DIR = 'photos_resized/photos_resized/'
+META_DATA_FILE = 'meta/image_meta.csv'
 IMG2SHOP_FILE = 'meta/photo_id_to_business_id.json'
 IMG_NAMES_FILE = 'meta/img_names.txt'
 IMG_Y_SIZE = 400
