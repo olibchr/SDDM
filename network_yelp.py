0b3f0c041e19ed1d700b9c979f32596f490df291
oibchr <oliver.becher@gmail.com>
2016-05-28 22:56:10 +0200

    bound

--- network_yelp.py
+++ network_yelp.py
@@ -82,7 +82,6 @@ def load_dataset():
             if img_id in dic:
                 X_imgs.append(face / np.float32(256))
                 y_imgs.append(dic[img_id])
-                print("Image %s with %s stars" %(img_id, y_imgs[-1]))
             else:
                 print('No entry for %s found!' % (img_id))
                 
