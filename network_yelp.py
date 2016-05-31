ec9aa78a596311bfe3f614a2c103571f9effddad
arthurlaurens <alvrooijen@gmail.com>
2016-05-31 11:25:58 +0200

    fix

--- network_yelp.py
+++ network_yelp.py
@@ -249,7 +249,8 @@ def main(model='cnn', num_epochs=200):
         if theano.tensor.gt(valid_error_prev - valid_error, LEARN_THRESH):
         # if LEARN_THRESH >= valid_error_prev - valid_error:
             LEARN_RATE = LEARN_CHANGE * LEARN_RATE
-            print "marginal improvement:" + str(valid_error_prev - valid_error) + ", change learn rate: " + str(thprint.Print(LEARN_RATE))
+            print "marginal improvement:" + str(valid_error_prev - valid_error) + ", change learn rate: "
+            print LEARN_RATE
 
 
         ##### EARLY STOPPING ####
