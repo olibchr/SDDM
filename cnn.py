35bd0cf853f9bdc4104d959cf6ebb7bb1f70030e
arthurlaurens <alvrooijen@gmail.com>
2016-05-31 13:36:07 +0200

    test

--- cnn.py
+++ cnn.py
@@ -14,7 +14,8 @@ def build(image_x_size, image_y_size, n_classes, input_var=None):
             network, num_filters=48, filter_size=(11, 11),
             stride=(4, 4),
             nonlinearity=lasagne.nonlinearities.rectify,
-            W=lasagne.init.Normal(weight_init_std, weight_init_mean))
+            W=lasagne.init.Normal(weight_init_std, weight_init_mean),
+            b=lasagne.utils.create_param(1, 48, name=None))
     network = lasagne.layers.LocalResponseNormalization2DLayer(network)
     network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
     # Expert note: Lasagne provides alternative convolutional layers that
