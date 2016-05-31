eb222fadad4649fd55ac7b3ac9e41ecf81620a2f
arthurlaurens <alvrooijen@gmail.com>
2016-05-31 11:35:18 +0200

    fix

--- network_yelp.py
+++ network_yelp.py
@@ -246,7 +246,7 @@ def main(model='cnn', num_epochs=200):
             val_acc / val_batches * 100))
 
         ### LEARN RATE CHANGE ###
-        if theano.tensor.gt(valid_error_prev - valid_error, LEARN_THRESH):
+        if theano.tensor.le((valid_error_prev - valid_error), LEARN_THRESH):
         # if LEARN_THRESH >= valid_error_prev - valid_error:
             LEARN_RATE = LEARN_CHANGE * LEARN_RATE
             print "marginal improvement:" + str(valid_error_prev - valid_error) + ", change learn rate: " + str(LEARN_RATE.eval())
