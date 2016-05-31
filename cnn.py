18ac07674caff2c3aec795db07f3949e424b92dd
oibchr <oliver.becher@gmail.com>
2016-05-31 10:40:56 +0200

    a

--- cnn.py
+++ cnn.py
@@ -33,15 +33,16 @@ def build(image_x_size, image_y_size, n_classes, input_var=None):
             network, num_filters=192, filter_size=(3, 3),
             nonlinearity=lasagne.nonlinearities.rectify)
 
-    # IMG_NET: 3x3 conv 13x13x128
+    # IMG_NET: 3x3 conv 13x13x128 + pool
     network = lasagne.layers.Conv2DLayer(
             network, num_filters=128, filter_size=(3, 3),
             nonlinearity=lasagne.nonlinearities.rectify)
+    network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
 
     # IMG_NET: dense 2048
     network = lasagne.layers.DenseLayer(
             lasagne.layers.dropout(network, p=.5),
-            num_units=256,
+            num_units=2048,
             nonlinearity=lasagne.nonlinearities.rectify)
 
     # IMG_NET: dense 2048
