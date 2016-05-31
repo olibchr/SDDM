fa768d76fa820f976a397c45a5c3b4f0ac55a754
arthurlaurens <alvrooijen@gmail.com>
2016-05-31 15:16:54 +0200

    no dropout  in last layer

--- cnn.py
+++ cnn.py
@@ -72,7 +72,6 @@ def build(image_x_size, image_y_size, n_classes, input_var=None):
 
     # Output Layer
     network = lasagne.layers.DenseLayer(
-            lasagne.layers.dropout(network, p=.5),
             num_units=n_classes,
             nonlinearity=lasagne.nonlinearities.softmax,
             W=lasagne.init.Normal(weight_init_std, weight_init_mean),
