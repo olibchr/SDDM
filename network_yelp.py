cddffcaeefd60dd055d974fb159fb5c683043508
arthurlaurens <alvrooijen@gmail.com>
2016-05-31 11:32:52 +0200

    fix

--- network_yelp.py
+++ network_yelp.py
@@ -9,9 +9,7 @@ from scipy import misc
 # neural network imports
 import theano
 import theano.tensor as T
-import theano.printing as thprint
 import lasagne
-from PIL import Image
 
 
 # our imports
@@ -246,8 +244,7 @@ def main(model='cnn', num_epochs=200):
             val_acc / val_batches * 100))
 
         ### LEARN RATE CHANGE ###
-        if theano.tensor.gt(valid_error_prev - valid_error, LEARN_THRESH):
-        # if LEARN_THRESH >= valid_error_prev - valid_error:
+        if theano.tensor.le(valid_error_prev - valid_error, LEARN_THRESH):
             LEARN_RATE = LEARN_CHANGE * LEARN_RATE
             print "marginal improvement:" + str(valid_error_prev - valid_error) + ", change learn rate: " + str(LEARN_RATE.eval())
 
