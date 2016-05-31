e76b1054db8058bea4e49b5cb162f46155964d5b
oibchr <oliver.becher@gmail.com>
2016-05-31 17:37:36 +0200

    debug

--- network_yelp.py
+++ network_yelp.py
@@ -84,10 +84,10 @@ def images_to_mem(image_idx):
             if img_id in dic:
                 X_imgs.append(face / np.float32(256))
                 #X_imgs.append(face_lr / np.float32(256))
-                #y_vec = [0] * N_CLASSES
-                #y_vec[dic[img_id]] = 1
-                #print(y_vec)
-                #print(dic[img_id])
+                y_vec = [0] * N_CLASSES
+                y_vec[dic[img_id]] = 1
+                print(y_vec)
+                print(dic[img_id])
                 #y_imgs.append(y_vec)
                 #y_imgs.append(y_vec)
                 y_imgs.append(dic[img_id])
