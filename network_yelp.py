1031b7e6da4676a8bb1d3022c8d5440ba08504b5
oibchr <oliver.becher@gmail.com>
2016-05-31 17:38:57 +0200

    debug

--- network_yelp.py
+++ network_yelp.py
@@ -85,9 +85,9 @@ def images_to_mem(image_idx):
                 X_imgs.append(face / np.float32(256))
                 #X_imgs.append(face_lr / np.float32(256))
                 y_vec = [0] * N_CLASSES
-                y_vec[dic[img_id]] = 1
+                #y_vec[dic[img_id]] = 1
                 print(y_vec)
-                print(dic[img_id])
+                #print(dic[img_id])
                 #y_imgs.append(y_vec)
                 #y_imgs.append(y_vec)
                 y_imgs.append(dic[img_id])
